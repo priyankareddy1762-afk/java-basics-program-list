684c52c255e6f60218c8e58997fdb46e6a32ae0f
priyankareddy1762-afk <priyankareddy.1762@gmail.com>
2026-08-13 23:32:09 +0530

    add two numbers

---
+++ 01-java-basics/Add_nums.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Add_nums{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int m= sc.nextInt();
+        System.out.println(m+n);
+        sc.close();
+    }
+}
