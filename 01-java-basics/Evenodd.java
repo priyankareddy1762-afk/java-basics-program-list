58fe50f0eda4a96fd8452e908adc35b7dfcb19f9
priyankareddy1762-afk <priyankareddy.1762@gmail.com>
2026-08-13 23:42:28 +0530

    check even or odd

---
+++ 01-java-basics/Evenodd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Evenodd {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("evem");
+        }
+        else{
+            System.out.println("odd");
+        }
+    sc.close();
+}
+}
