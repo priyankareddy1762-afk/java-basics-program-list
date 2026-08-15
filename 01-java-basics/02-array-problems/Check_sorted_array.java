c5aae0caeae3e096cfb7d86798efff9c5db89c03
priyankareddy1762-afk <priyankareddy.1762@gmail.com>
2026-08-15 22:18:20 +0530

    checking if an array is sorted

---
+++ 01-java-basics/02-array-problems/Check_sorted_array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Check_sorted_array{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        boolean sorted=true;
+        for(int i=1;i<n;i++){
+            if(arr[i]<arr[i-1]){
+               sorted=false;
+                break;
+
+            }
+
+        }
+        System.out.println(sorted);
+        sc.close();
+
+    }
+}
